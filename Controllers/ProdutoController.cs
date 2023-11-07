b323efaab99e26096cb751c82038af7099b0cabb
Jeyza <santana.jeyza@escolar.ifrn.edu.br>
2023-11-07 09:43:30 -0300

    jeyzinha

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiMaryTech.Models;
+using ApiMaryTech.Context;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiMaryTech.Controllers
@@ -11,33 +12,69 @@ namespace ApiMaryTech.Controllers
     [Route("api/[controller]")]
     public class ProdutoController : ControllerBase
     {
-        [HttpGet(Name = "produtos")]
-        public ActionResult<IEnumerable<Produto>> GetProdutos()
+        private readonly ILogger<ProdutoController> _logger;
+        private readonly ApiMaryTechContext _context; 
+
+        public ProdutoController (ILogger<ProdutoController> logger, ApiMaryTechContext context)
         {
-            List<Produto> produtos = new List<Produto>();
+            _logger = logger;
+            _context = context;
+        }
 
+    [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produto.ToList();
+            if(produtos is null)
+                return NotFound();
             
-            produtos.Add(new Produto
-            {   
-                Id= 2,
-                Descricao = "Caneca vingadores",
-                Tamanho = "15 cm",
-                Peso = "10 g",
-                Cor = "Personalizada",
-                Valor = 30
-            });
-
-            produtos.Add(new Produto
-            {
-                Id = 9,
-                Descricao = "Caderno do PT",
-                Tamanho = "21 cm x 30 cm",
-                Peso = "200 g",
-                Cor = "Vermelho",
-                Valor = 50
-            });
-
             return produtos;
+        }
+
+        [HttpGet("{id:int}", Name ="GetProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produtos = _context.Produto.FirstOrDefault(p => p.Id == id);
+            if(produtos is null)
+                return NotFound("Produto não encontrado");
+            
+            return disciplina;
+        }
+
+        
+        [HttpPost]
+        public ActionResult Post(Disciplina disciplinas){
+            _context.Disciplinas.Add(disciplinas);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetDisciplinas",
+                new{id = disciplinas.Id},
+                disciplinas);
+                
+        }
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Disciplina disciplina){
+            if(id != disciplina.Id)
+                return BadRequest();
+
+            _context.Entry(disciplina).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(disciplina);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id){
+            var disciplina = _context.Disciplinas.FirstOrDefault(p => p.Id == id);
+            
+            if(disciplina is null) 
+                return NotFound();
+
+            _context.Disciplinas.Remove(disciplina);
+            _context.SaveChanges();
+
+            return Ok(disciplina);
+        }
+
     }
-  }
 }
