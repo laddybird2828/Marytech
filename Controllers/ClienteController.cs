eeb2a2551cd6eed613dc7496d208e1e7a071e75c
iluska <iluska.mellina@gmail.com>
2023-11-07 09:31:17 -0300

    Comecei a fazer minha parte no CRUD

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -2,34 +2,79 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ApiMaryTech.Models;
+using apiMaryTechContex.Model;
+using apiMaryTechContex.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace ApiMaryTech.Controllers
+namespace apiMaryTechContex.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class ClienteController : ControllerBase
     {
-        [HttpGet(Name = "clientes")]
-        public ActionResult<IEnumerable<Cliente>> GetClientes()
+        private readonly ILogger<AlunoController> _logger;
+        private readonly ApiUniversidadeContext _context;
+
+        public AlunoController(ILogger<AlunoController> logger, ApiUniversidadeContext context)
         {
-            List<Cliente> clientes = new List<Cliente>();
-
-        
-            clientes.Add(new Cliente
-            {   Id = 22,
-                Nome = "Taylor Swift",
-                NumeroWhatsapp = "5511999999999"
-            });
-
-            clientes.Add(new Cliente
-            {   Id = 666,
-                Nome = "Jhon Lennon",
-                NumeroWhatsapp = "5511888888888"
-            });
-
-            return clientes;
+            _logger = logger;
+            _context = context;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Aluno>> Get()
+        {
+           var alunos = _context.Alunos.ToList();
+           if(alunos is null)
+                return NotFound();
+
+            return alunos;
+        }
+
+        [HttpGet("{id:int}", Name="GetAluno")]
+        public ActionResult<Aluno> Get(int id)
+        {
+           var alunos = _context.Alunos.FirstOrDefault(p => p.Id == id);
+           if(alunos is null)
+                return NotFound("Aluno não encontrado.");
+
+            return alunos;
+        }
+ 
+        [HttpPost]
+        public ActionResult Post(Aluno aluno){
+            _context.Alunos.Add(aluno);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetAluno", 
+                new{ id = aluno.Id},
+                aluno);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Aluno aluno){
+            if(id != aluno.Id)
+                return BadRequest();
+
+            _context.Entry(aluno).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id){
+            var aluno = _context.Alunos.FirstOrDefault(p => p.Id == id);
+
+            if(aluno is null)
+                return NotFound();
+
+            _context.Alunos.Remove(aluno);
+            _context.SaveChanges();
+
+            return Ok(aluno);
+        }
+
     }
 }
